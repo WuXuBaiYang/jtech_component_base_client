d9b5d7e3a1e26758a710e267aaf54344701ae39d
j <wuxubaiyang@gmail.com>
2021-07-02 15:01:49 +0800

    添加消息总线管理对象

--- jtech_base_library/lib/event/jevent.dart
+++ jtech_base_library/lib/event/jevent.dart
@@ -2,6 +2,9 @@ import 'package:event_bus/event_bus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:jtech_base_library/event/base_event_model.dart';
 
+//注册事件的消息回调
+typedef OnEventListen = void Function<T extends BaseEventModel>(T event);
+
 /*
 * 消息总线管理
 * @author wuxubaiyang
@@ -16,15 +19,23 @@ class JEvent {
   JEvent._internal();
 
   //消息总线对象
-  final eventBus = EventBus();
+  final eventBus = EventBus(sync: true);
 
   //注册事件
-  Future<void> on<T extends BaseEventModel>() async {
-    eventBus.on<T>();
+  void on<T extends BaseEventModel>({required OnEventListen listen}) {
+    eventBus.on<T>().listen((event) {
+      listen(event);
+    });
   }
-  //发送事件
-  Future<void> send()async{
 
+  //注册单次事件
+  Future<T> onOnce<T extends BaseEventModel>() async {
+    return eventBus.on<T>().first;
+  }
+
+  //发送事件
+  void send<T extends BaseEventModel>(T event) async {
+    eventBus.fire(event);
   }
 }
 
--- jtech_base_library/test/widget_test.dart
+++ jtech_base_library/test/widget_test.dart
@@ -2,12 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jtech_base_library/base/base_page.dart';
+import 'package:jtech_base_library/event/base_event_model.dart';
 import 'package:jtech_base_library/event/jevent.dart';
 
 void main() {
   testWidgets("基类测试", (tester) async {
     tester.pumpWidget(MyApp());
   });
+
+  test("消息总线测试", () {
+    jEvent.send(CusEvent("测试方法"));
+    jEvent.onOnce().then((value) {
+      print("");
+    });
+  });
+}
+
+class CusEvent extends BaseEventModel {
+  String text = "";
+
+  CusEvent(this.text);
 }
 
 //测试基本页面
