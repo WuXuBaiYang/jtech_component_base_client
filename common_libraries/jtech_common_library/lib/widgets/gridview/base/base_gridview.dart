f11b615d8c3f0adb203f8546409e62c952866b73
j <wuxubaiyang@gmail.com>
2021-07-19 16:07:37 +0800

    表格列表组件创建

---
+++ common_libraries/jtech_common_library/lib/widgets/gridview/base/base_gridview.dart
@@ -0,0 +1,8 @@
+import 'package:jtech_base_library/base/base_stateful_widget.dart';
+
+/*
+* 表格列表组件基类
+* @author wuxubaiyang
+* @Time 2021/7/19 下午4:06
+*/
+abstract class BaseGridView extends BaseStatefulWidget {}
---
+++ common_libraries/jtech_common_library/lib/widgets/gridview/base/controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+/*
+* 表格列表控制器
+* @author wuxubaiyang
+* @Time 2021/7/19 下午4:05
+*/
+class GridViewController {
+
+}
