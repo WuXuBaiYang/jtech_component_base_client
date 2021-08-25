b9dcf72187d7d961063cf28d7edf9437a89dc474
jh <wuxubaiyang@gmail.com>
2021-08-25 14:47:00 +0800

    +1

--- common_libraries/jtech_common_library/test/widget_test.dart
+++ common_libraries/jtech_common_library/test/widget_test.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jtech_common_library/jcommon.dart';
+
+void main() {
+  test("测试url拼接", () {
+    var a = "pathA";
+    var b = "fileName.jpg";
+    var c = join(a, b);
+    print("$c");
+  });
+}
