e13452f34bf81ab6d5f3fce5696ea2a11642ccfb
jh <wuxubaiyang@gmail.com>
2021-09-07 14:32:32 +0800

    model增加json转换方法

--- jtech_base_library/lib/base/base_model.dart
+++ jtech_base_library/lib/base/base_model.dart
@@ -1,6 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
 /*
 * 数据对象基类
 * @author jtechjh
 * @Time 2021/8/13 8:56 上午
 */
-abstract class BaseModel {}
+abstract class BaseModel {
+  //解析json
+  BaseModel fromJson(Map<String, dynamic> json) => this;
+
+  //从json字符串解析
+  @protected
+  BaseModel fromJsonString(String json) => fromJson(jsonDecode(json));
+
+  //转化json
+  Map<String, dynamic> toJson() => {};
+
+  //转化为json字符串
+  @protected
+  String toJsonString() => jsonEncode(toJson());
+}
